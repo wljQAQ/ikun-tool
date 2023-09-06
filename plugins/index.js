a46af516e9ca4ec1ae9484852389e62b15170930
花开富贵 <71117867+wljQAQ@users.noreply.github.com>
2023-09-06 17:58:02 +0800

    webpack配置

--- plugins/index.js
+++ plugins/index.js
@@ -1,14 +1,10 @@
-const { spawn } = require("child_process");
+const { spawn, exec } = require("child_process");
 
 //打包完后执行cmd命令
 class IkunPlugun {
   apply(compiler) {
     compiler.hooks.afterEmit.tapAsync("IkunPlugun", (compilation, callback) => {
-      const cmd = spawn("npx", ["ikun-tool"], { shell: true });
-
-      cmd.stdout.on("data", (data) => {
-        console.log(`${data}`);
-      });
+      spawn("npx", ["ikun-tool"], { shell: true, stdio: "inherit" });
 
       callback();
     });
