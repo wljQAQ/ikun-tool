ff9c01a39576cec17f39b98bd0ae58b62f96cc78
花开富贵 <71117867+wljQAQ@users.noreply.github.com>
2023-10-09 17:36:07 +0800

    '文件复制完成'

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -24,24 +24,26 @@ const COLOR_MAP = {
   react: cyan,
 };
 
+const TEMPLATE_PATH = path.resolve(fileURLToPath(import.meta.url), "../../templates");
+
 const argv = minimist<Argv>(process.argv.slice(2));
 const cwd = process.cwd();
 
 //大致逻辑 如果有提供名称跟template名称就直接拷贝模板进去 否则就进入对话让用户选择
 async function init() {
-  const name = argv._[0];
-  const templateName = argv.template || argv.t;
+  const argv_name = argv._[0];
+  const argv_template = argv.template || argv.t;
   //进入对话
   const templates = parseTemplates();
-  const result: prompts.Answers<"projectName" | "template"> = await prompts([
+  const result: prompts.Answers<"prompts_name" | "prompts_template"> = await prompts([
     {
-      type: name ? null : "text",
-      name: "projectName",
+      type: argv_name ? null : "text",
+      name: "prompts_name",
       message: reset("请输入你的项目名称"),
     },
     {
-      type: templateName ? null : "select",
-      name: "template",
+      type: argv_template ? null : "select",
+      name: "prompts_template",
       message: reset("请选择一个项目模板"),
       choices: templates.map((i) => {
         return {
@@ -52,21 +54,25 @@ async function init() {
     },
   ]);
 
-  const { template } = result;
+  const { prompts_name, prompts_template } = result;
+  const name = argv_name || prompts_name;
+  const template = argv_template || prompts_template;
+  const src = path.resolve(TEMPLATE_PATH, template.toString());
+  const dest = path.resolve(cwd, name.toString());
 
+  //
+  if (!fs.existsSync(dest)) fs.mkdirSync(dest);
   //接着就是拷贝模板到目标目录
+  copy(src, dest);
 }
 
 //拷贝又分为 拷贝目录跟拷贝文件
 function copy(src: string, dest: string) {
-  const isDirectory = fs.statSync(src).isDirectory();
+  const isDirectory = fs.statSync(src)?.isDirectory();
 
   //如果是文件夹
-  if (isDirectory) {
-    copyDir(src, dest);
-  } else {
-    fs.copyFileSync(src, dest);
-  }
+  if (isDirectory) copyDir(src, dest);
+  else fs.copyFileSync(src, dest);
 }
 
 function copyDir(src: string, dest: string) {
@@ -83,10 +89,8 @@ function copyDir(src: string, dest: string) {
 }
 
 function parseTemplates(): Templates {
-  const root = fileURLToPath(import.meta.url);
-  const templateRoot = path.join(root, "../../templates");
   let result: Templates = [];
-  const files = fs.readdirSync(templateRoot);
+  const files = fs.readdirSync(TEMPLATE_PATH);
   result = files.map((i) => {
     const startStr = i.split("-")[0];
     return {
